809056071e2d1e5958b2070ca59e5822a6c2b51b
Roman Fursov <roman@fursov.eu>
2025-06-15 15:27:32 +0200

    Simplified page contents, removed a link

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,3 @@
-const facts = [
-    "I have two Maine Coons",
-    "I have traveled to over 20 countries",
-    "I enjoy learning new languages",
-    "I can bake a decent bread",
-    "I fed a giraffe once",
-];
-
 window.onload = function () {
     setTimeout(function () {
         document.querySelector('.page__images').classList.add('page__images_visible');
@@ -14,20 +6,4 @@ window.onload = function () {
     setTimeout(function () {
         document.querySelector('.page__content').classList.add('page__content_visible');
     }, 700);
-
-    let currentIndex = 0;
-
-    function replaceFact() {
-        const factElement =  document.querySelector(".page__random_fact");
-        const factText = factElement.querySelector("#fact");
-        factElement.style.opacity = 0;
-        setTimeout(() => {
-            currentIndex = (currentIndex + 1) % facts.length;
-            factText.textContent = facts[currentIndex];
-            factElement.style.opacity = 1;
-        }, 500);
-    }
-
-    document.querySelector(".page__random_fact").addEventListener("click", replaceFact);
-    replaceFact();
 };
